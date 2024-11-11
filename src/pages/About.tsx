8541326729fae3ac7b36bb673ee6a128f36882a7
Paige Braun <paigembraun@gmail.com>
2024-11-11 16:47:15 -0600

    update packages and about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,25 @@
 import Contact from "../components/Contact";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
 function About() {
     return (
-        <div className='max-w-3xl flex-1 text-xs'>
-            <p className='mb-2'>I’m a <span className="font-bold">front end developer</span> with a background in design and a passion for creative problem solving.</p>
-            <p className='mb-2'>I’ve always been fascinated by how things work. This curiosity led me to study film and computer science at the University of Texas at Austin.</p>
-            <p className='mb-2'>I currently work as a video producer and I’m looking to leverage my technical and creative skills to transition into a front end development role. I love using technology to recognize patterns and improve efficiency and I’m excited to be a part of a team that shares this passion.</p>
-            <p className='mb-2'>I design and develop all of my own projects. Check them out using the nav bar on the left or top of your screen.</p>
-            <h3 className='font-bold mb-4 mt-6'>Skills</h3>
-            <div className='grid grid-cols-2 grid-rows-7 max-w-56'>
+        <div className="max-w-3xl flex-1 text-xs">
+            <p className="mb-2">
+                I'm a <span className="font-bold">front end developer</span>{" "}
+                with a background in design and a passion for creative problem
+                solving.
+            </p>
+            <p className="mb-2">
+                I've always been fascinated by how things work. This curiosity
+                led me to study film and computer science at the University of
+                Texas at Austin.
+            </p>
+            <p className="mb-2">
+                I currently work at Visa on their design system team. I love
+                using technology to recognize patterns and improve efficiency.
+            </p>
+            <h3 className="font-bold mb-4 mt-6">Skills</h3>
+            <div className="grid grid-cols-2 grid-rows-7 max-w-56">
                 <p>TypeScript</p>
                 <p>React</p>
                 <p>JavaScript</p>
@@ -29,14 +39,14 @@ function About() {
                 <p>After Effects</p>
                 <p>Figma</p>
             </div>
-            <h3 className='font-bold mb-4 mt-6'>Education</h3>
-            <p className='mb-2'>University of Texas at Austin</p>
+            <h3 className="font-bold mb-4 mt-6">Education</h3>
+            <p className="mb-2">University of Texas at Austin</p>
             <p>Bachelor of Science in Radio-Television-Film</p>
             <p>Elements of Computing Certificate</p>
             <Contact />
             <Footer />
         </div>
-    )
+    );
 }
 
 export default About;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,131 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import projectInfo, { Project } from '../modules/projectInfo';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import NotFound from './NotFound';
-import Popup from '../components/Popup';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import projectInfo, { Project } from "../modules/projectInfo";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import NotFound from "./NotFound";
+import Popup from "../components/Popup";
 
-import {MdOpenInNew} from 'react-icons/md';
+import { MdOpenInNew } from "react-icons/md";
 
 function toTitleCase(str: string) {
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
-  });
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
+    });
 }
 
 function formatProjectName(name: string) {
-  if (name.toLowerCase() === 'wnba swish') {
-    return 'WNBA Swish';
-  }
-  return toTitleCase(name);
+    if (name.toLowerCase() === "wnba swish") {
+        return "WNBA Swish";
+    }
+    return toTitleCase(name);
 }
 
 function Projects() {
-  const { projectId } = useParams<{ projectId: string }>();
-  const project: Project | undefined = projectInfo.find((project) => project.id === projectId);
+    const { projectId } = useParams<{ projectId: string }>();
+    const project: Project | undefined = projectInfo.find(
+        (project) => project.id === projectId
+    );
 
-  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
-  const [imageLoaded, setImageLoaded] = useState<boolean[]>([]);
+    const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
+        null
+    );
+    const [imageLoaded, setImageLoaded] = useState<boolean[]>([]);
 
-  useEffect(() => {
-    if (selectedImageIndex !== null) {
-      document.body.classList.add('overflow-hidden');
-    } else {
-      document.body.classList.remove('overflow-hidden');
-    }
-    return () => {
-      document.body.classList.remove('overflow-hidden');
+    useEffect(() => {
+        if (selectedImageIndex !== null) {
+            document.body.classList.add("overflow-hidden");
+        } else {
+            document.body.classList.remove("overflow-hidden");
+        }
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, [selectedImageIndex]);
+
+    const openImage = (index: number) => {
+        setSelectedImageIndex(index);
     };
-  }, [selectedImageIndex]);
 
-  const openImage = (index: number) => {
-    setSelectedImageIndex(index);
-  };
+    const closeImage = () => {
+        setSelectedImageIndex(null);
+    };
 
-  const closeImage = () => {
-    setSelectedImageIndex(null);
-  };
+    const openNextImage = () => {
+        if (project?.src) {
+            const currentIndex = selectedImageIndex ?? 0;
+            setSelectedImageIndex((currentIndex + 1) % project.src.length);
+        }
+    };
 
-  const openNextImage = () => {
-    if (project?.src) {
-      const currentIndex = selectedImageIndex ?? 0;
-      setSelectedImageIndex((currentIndex + 1) % project.src.length);
+    const openPrevImage = () => {
+        if (project?.src) {
+            const currentIndex = selectedImageIndex ?? 0;
+            setSelectedImageIndex(
+                (currentIndex - 1 + project.src.length) % project.src.length
+            );
+        }
+    };
+
+    const handleImageLoad = (index: number) => {
+        setImageLoaded((prevLoaded) => {
+            const newLoaded = [...prevLoaded];
+            newLoaded[index] = true;
+            return newLoaded;
+        });
+    };
+
+    if (!project) {
+        return <NotFound />;
     }
-  };
-  
-  const openPrevImage = () => {
-    if (project?.src) {
-      const currentIndex = selectedImageIndex ?? 0;
-      setSelectedImageIndex((currentIndex - 1 + project.src.length) % project.src.length);
-    }
-  };
 
-  const handleImageLoad = (index: number) => {
-    setImageLoaded((prevLoaded) => {
-      const newLoaded = [...prevLoaded];
-      newLoaded[index] = true;
-      return newLoaded;
-    });
-  };
-
-  if (!project) {
-    return <NotFound />;
-  }
-
-  return (
-    <div className='max-w-3xl flex-1 text-xs'>
-      <h3 className='font-bold mb-4'>{formatProjectName(project.name)}</h3>
-      <p className='mb-4'>{project.description.split('\n').map((line, index) => (
-        <React.Fragment key={index}>
-          {line}
-          <br />
-        </React.Fragment>
-      ))}</p>
-      <p className='mb-4'><span className="font-bold">Tech: </span>{project.tech}</p>
-      {project.livePreview && (
-        <div className='flex gap-4'>
-          <a className='font-bold flex items-center mb-2 hover:underline' href={project.livePreview} target='_blank' rel="noopener noreferrer">Live Preview <MdOpenInNew className='ml-2'/></a>
-          <a className='font-bold flex items-center mb-2 hover:underline' href={project.code} target='_blank' rel="noopener noreferrer">Code <MdOpenInNew className='ml-2'/></a>
+    return (
+        <div className="max-w-3xl flex-1 text-xs">
+            <h3 className="font-bold mb-4">
+                {formatProjectName(project.name)}
+            </h3>
+            <p className="mb-4">
+                {project.description.split("\n").map((line, index) => (
+                    <React.Fragment key={index}>
+                        {line}
+                        <br />
+                    </React.Fragment>
+                ))}
+            </p>
+            <p className="mb-4">
+                <span className="font-bold">Tech: </span>
+                {project.tech}
+            </p>
+            {project.livePreview && (
+                <div className="flex gap-4">
+                    <a
+                        className="font-bold flex items-center mb-2 hover:underline"
+                        href={project.livePreview}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Live Preview <MdOpenInNew className="ml-1" />
+                    </a>
+                    <a
+                        className="font-bold flex items-center mb-2 hover:underline"
+                        href={project.code}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Code <MdOpenInNew className="ml-1" />
+                    </a>
+                </div>
+            )}
+            {!project.livePreview && (
+                <a
+                    className="font-bold flex items-center mb-2 hover:underline"
+                    href={project.code}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Code <MdOpenInNew className="ml-1" />
+                </a>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                {project.src?.map((img, index) => (
+                    <img
+                        key={index}
+                        src={img}
+                        onClick={() => openImage(index)}
+                        onLoad={() => handleImageLoad(index)}
+                        alt={`Image ${index + 1}`}
+                        className="cursor-pointer"
+                        style={{
+                            opacity: imageLoaded[index] ? 1 : 0,
+                            transition: "opacity 0.5s",
+                            width: "100%",
+                            height: "auto",
+                        }}
+                    />
+                ))}
+            </div>
+            {selectedImageIndex !== null && project.src && (
+                <Popup
+                    isOpen={selectedImageIndex !== null}
+                    src={project.src[selectedImageIndex]}
+                    onClose={closeImage}
+                    currentIndex={selectedImageIndex}
+                    totalImages={project.src.length}
+                    nextImage={openNextImage}
+                    prevImage={openPrevImage}
+                    showCounter={false}
+                />
+            )}
+            <Contact />
+            <Footer />
         </div>
-      )}
-      {!project.livePreview && (
-        <a className='font-bold flex items-center mb-2 hover:underline' href={project.code} target='_blank' rel="noopener noreferrer">Code</a>
-      )}
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-4'>
-        {project.src?.map((img, index) => (
-          <img
-            key={index}
-            src={img}
-            onClick={() => openImage(index)}
-            onLoad={() => handleImageLoad(index)}
-            alt={`Image ${index + 1}`}
-            className='cursor-pointer'
-            style={{
-              opacity: imageLoaded[index] ? 1 : 0,
-              transition: 'opacity 0.5s',
-              width: '100%',
-              height: 'auto'
-            }}
-          />
-        ))}
-      </div>
-      {selectedImageIndex !== null && project.src && (
-        <Popup
-          isOpen={selectedImageIndex !== null}
-          src={project.src[selectedImageIndex]}
-          onClose={closeImage}
-          currentIndex={selectedImageIndex}
-          totalImages={project.src.length}
-          nextImage={openNextImage}
-          prevImage={openPrevImage}
-          showCounter={false}
-        />
-      )}
-      <Contact />
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default Projects;
